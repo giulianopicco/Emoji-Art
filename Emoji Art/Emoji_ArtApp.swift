845028be720d63343b6c13d0313a526023c8794e
maxXx <giulianopicco@gmail.com>
2024-08-27 17:56:21 -0400

    Lesson 11 completed

--- Emoji Art/Emoji_ArtApp.swift
+++ Emoji Art/Emoji_ArtApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct Emoji_ArtApp: App {
+    @StateObject var defaultDocument = EmojiArtDocument()
+    @StateObject var paletteStore = PaletteStore(named: "Main")
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmojiArtDocumentView(document: defaultDocument)
+                .environmentObject(paletteStore)
         }
     }
 }
